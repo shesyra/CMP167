37aeec7341fd23d933916d9b84c25b22e4f07a1c
shesyra <43551842+shesyra@users.noreply.github.com>
2018-09-24 14:50:59 -0400

    Add files via upload
    need help with this

---
+++ HelloTen.java
@@ -0,0 +1,12 @@
+public class HelloTen
+{
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 10;
+		while(a<=b)
+		{
+			System.out.println("Hello World");
+            a++;//a=a+1 or a+=1
+		}
+	}
+}
